be20301f1b07252a29e5ee128b668fb9b828db14
Natanael Aguiar <natanaelhaguiar@gmail.com>
2018-10-27 17:12:00 -0300

    Resolve o bug que estava pegando somente um record do client_bottles

--- controleGarrafao/app/models/client.rb
+++ controleGarrafao/app/models/client.rb
@@ -3,10 +3,10 @@ class Client < ApplicationRecord
   has_many :bottles, :through => :client_bottles
   validates :street,:number,:client_bottles, presence: true
 
-  accepts_nested_attributes_for :client_bottles
+  accepts_nested_attributes_for :client_bottles, allow_destroy: true
 
   def client_bottles_names
-    client_bottles = Array(ClientBottle.find_by(client_id: id))
+    client_bottles = Array(ClientBottle.where(client_id: id).limit(nil))
     client_bottles = client_bottles.map do |client_bottle|
       client_bottle = client_bottle.attributes.merge!({"bottle_name": Bottle.find(client_bottle.bottle_id).name})
       client_bottle = client_bottle.except("client_id","created_at","updated_at")
